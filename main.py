f773026f6aaf1f755e4e7f2afc9581ae2cc22a76
wenjun <hi@awen.me>
2020-08-28 20:44:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+#-*-coding:utf-8-*-
+
 from random import choice
 from string import digits
+from urllib.parse import quote_plus
 from requests import get
 from time import sleep
+from os.path import exists
+from os import mkdir
 
-def get_images(pageno,count):
-    base_filepath = '/Users/wenjun/Pictures/meizhi/'
-    base_url = ' http://wp.birdpaper.cn/intf/GetListByHotTag?tag=%E6%B8%85%E7%BA%AF&pageno={}&count={}'.format(pageno,count)
+def get_images(tag,pageno,count):
+    base_filepath = 'C:/wallpaper/{}/'.format(tag)
+    if exists(base_filepath):
+        pass
+    else:
+        mkdir(base_filepath)
+    base_url = 'http://wp.birdpaper.cn/intf/GetListByHotTag?tag={}&pageno={}&count={}'.format(quote_plus(tag), pageno, count)
     headers = {
       'Host': 'wp.birdpaper.cn',
       'Proxy-Connection': 'keep-alive',
@@ -25,7 +34,7 @@ def get_images(pageno,count):
             url_list.append(url)
     for img in url_list:
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'
         }
         response = get(img,headers=headers,timeout=60)
         name = "".join(map(lambda x:choice(digits), range(20)))
@@ -36,6 +45,6 @@ if __name__ == '__main__':
 
     for i in range(1, 200):
         print("===== 开始下载第{}页 =====".format(i))
-        get_images(i, 10)
-        sleep(10)
+        get_images(tag="清纯",pageno=i, count=10)
+        sleep(60)
 
